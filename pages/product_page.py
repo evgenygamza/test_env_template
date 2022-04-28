2e1065159ae165803c389edb80ddb4d5e8740e26
evgenygamza <1i4m5g2@#$>
2022-04-28 13:57:21 +0300

    some functions moved to BasePage

--- pages/product_page.py
+++ pages/product_page.py
@@ -16,7 +16,15 @@ class ProductPage(BasePage):
         add_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BTN)
         add_btn.click()
         self.solve_quiz_and_get_code()
+
+    def should_product_in_cart(self):
         cart_price = self.browser.find_element(*ProductPageLocators.CART_SUM).text
         book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
         assert '0.00' not in cart_price, 'cart is empty!'
         assert 'Coders at Work' == book_name, 'incorrect book name!'
+
+    def check_not_the_success_mes(self):
+        assert self.is_not_element_present(*ProductPageLocators.BOOK_NAME), 'success message exists!'
+
+    def check_success_mes_id_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.BOOK_NAME), 'success message didn`t disappeared!'
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@
 __Author__ = 'Gamza'
 import pytest
 from .pages.product_page import ProductPage
-
+from .pages.login_page import LoginPage
 
 def test_should_see_addtocart_btn(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
@@ -13,9 +13,48 @@ def test_should_see_addtocart_btn(browser):
 
 @pytest.mark.parametrize('link_tail', [0,1,2,3,4,5,6,pytest.param(7, marks=pytest.mark.xfail),8,9])
 def test_add_to_cart(browser, link_tail):
-    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link_tail}'
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.should_add_to_cart()
+    page.should_product_in_cart()
 
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_add_to_cart()
+    page.check_not_the_success_mes()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)
+    page.open()
+    page.check_not_the_success_mes()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_add_to_cart()
+    page.check_success_mes_id_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
